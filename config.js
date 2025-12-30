6b2a36afd694dd63ffa38e47ce5b5cdbbe012922
logicalRecursion <markstrongjr@gmail.com>
2025-12-29 19:46:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL";
-export const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_PUBLIC_KEY";
+export const SUPABASE_URL = "https://oduaietioqzzelfcdmvw.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_ui46xBriNISkMxspD45sOQ_MH-EpUtT";
