09d42b639e06ec8ea97f7eb82bfab1ee63c2db13
logicalRecursion <markstrongjr@gmail.com>
2026-01-11 11:35:18 -0500

    Add files via upload

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -4,22 +4,7 @@
 
   const sb = SupabaseClient.getClient();
 
-  // Pages that do NOT require auth
-  const PUBLIC_PAGES = [
-    "login.html",
-    "signup.html",
-    "congrats.html",
-    "404.html"
-  ];
-
-  function isPublicPage() {
-    const path = window.location.pathname;
-    return PUBLIC_PAGES.some(p => path.endsWith(p));
-  }
-
   async function requireAuth() {
-    if (isPublicPage()) return;
-
     const { data } = await sb.auth.getSession();
 
     if (!data.session) {
